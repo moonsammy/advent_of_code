8f8bfc956c5dadbfa1e723cc4e5d5e717ba32f23
scott milner <milner.scott@gmail.com>
2023-05-29 20:35:14 -0400

    day 5 part 1 only

--- 2022/d05/solution.py
+++ 2022/d05/solution.py
@@ -27,11 +27,81 @@ def read_data(file_name: str) -> List[str]:
 def solve() -> None:
   """ Solve part 1 and print the answer.
   """
-  pass
+  # stacks: List = [ ['Z','N'], ['M','C','D'], ['P'] ]
+  # instructions_list: List[str] = ['move 1 from 2 to 1',
+  #                                 'move 3 from 1 to 3',
+  #                                 'move 2 from 2 to 1',
+  #                                 'move 1 from 1 to 2']
+  #         [G]         [D]     [Q]
+  # [P]     [T]         [L] [M] [Z]
+  # [Z] [Z] [C]         [Z] [G] [W]
+  # [M] [B] [F]         [P] [C] [H] [N]
+  # [T] [S] [R]     [H] [W] [R] [L] [W]
+  # [R] [T] [Q] [Z] [R] [S] [Z] [F] [P]
+  # [C] [N] [H] [R] [N] [H] [D] [J] [Q]
+  # [N] [D] [M] [G] [Z] [F] [W] [S] [S]
+  #  1   2   3   4   5   6   7   8   9
+  instructions_list: List[str] = read_data('data.txt')
+  stacks: List = [['N','C','R','T','M','Z','P'],
+                  ['D','N','T','S','B','Z'],
+                  ['M','H','Q','R','F','C','T','G'],
+                  ['G','R','Z'],
+                  ['Z','N','R','H'],
+                  ['F','H','S','W','P','Z','L','D'],
+                  ['W','D','Z','R','C','G','M'],
+                  ['S','J','F','L','H','W','Z','Q'],
+                  ['S','Q','P','W','N']]
+
+  print(stacks[0][0], stacks[1][0], stacks[2][0], stacks[3][0], stacks[4][0], stacks[5][0], stacks[6][0], stacks[7][0], stacks[8][0])
+
+  for inst in instructions_list:
+    inst_list: List = inst.split(' ')
+    count: int = int(inst_list[1])
+    first_num: int = int(inst_list[3]) - 1
+    second_num: int = int(inst_list[5]) - 1
+    # Loop to move the n barrels
+    i: int = 0
+    while i < count:
+      if len(stacks[first_num]) > 0:
+        char_to_move: str = stacks[first_num].pop()
+        stacks[second_num].append(char_to_move)
+
+      i += 1
+  for z in stacks:
+    print(z)
+
 
 def solve_part_two() -> None:
   """ Solve part 2 and print the answer.
   """
+  # instructions_list: List[str] = read_data('data.txt')
+  # stacks: List = [['N','C','R','T','M','Z','P'],
+  #                 ['D','N','T','S','B','Z'],
+  #                 ['M','H','Q','R','F','C','T','G'],
+  #                 ['G','R','Z'],
+  #                 ['Z','N','R','H'],
+  #                 ['F','H','S','W','P','Z','L','D'],
+  #                 ['W','D','Z','R','C','G','M'],
+  #                 ['S','J','F','L','H','W','Z','Q'],
+  #                 ['S','Q','P','W','N']]
+
+  # print(stacks[0][0], stacks[1][0], stacks[2][0], stacks[3][0], stacks[4][0], stacks[5][0], stacks[6][0], stacks[7][0], stacks[8][0])
+
+  # for inst in instructions_list:
+  #   inst_list: List = inst.split(' ')
+  #   count: int = int(inst_list[1])
+  #   first_num: int = int(inst_list[3]) - 1
+  #   second_num: int = int(inst_list[5]) - 1
+  #   # Loop to move the n barrels
+  #   i: int = 0
+  #   while i < count:
+  #     if len(stacks[first_num]) > 0:
+  #       char_to_move: str = stacks[first_num].pop()
+  #       stacks[second_num].append(char_to_move)
+
+  #     i += 1
+  # for z in stacks:
+  #   print(z)
   pass
 
 if __name__ == '__main__':
